38bc36ee1e9d0cc2699a16df5cacea3aea571404
Ryan Drapeau <drapeau@cs.washington.edu>
2014-04-16 22:41:36 -0700

    Finished FileDate

---
+++ FileDate.py
@@ -0,0 +1,19 @@
+import time
+import os.path
+import datetime
+
+
+def get_modified_date(file_path):
+    return time.ctime(os.path.getmtime(file_path))
+
+
+def modified_recently(file_path, allowed_margin):
+    margin = datetime.timedelta(days=allowed_margin)
+    today = datetime.date.today()
+
+    modified = get_modified_date(file_path).split(' ')
+    modified = ' '.join(modified[:3]) + ' ' + modified[4]
+
+    modified_date = datetime.datetime.strptime(modified, "%a %b %d %Y").date()
+
+    return today - margin <= modified_date <= today + margin
